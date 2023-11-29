fb29708b94279f13f56e49b1242daa090215f5ac
Patrick Chan <q_p18@hotmail.com>
2023-11-29 08:36:02 +0000

    Create a CredentialsReader class and read_db_creds method.

---
+++ database_utils.py
@@ -0,0 +1,22 @@
+import yaml
+
+
+class CredentialsReader:
+    '''
+    This class is used to represent a credentials reader.
+    '''
+    @staticmethod
+    def read_db_creds(self, credentials_filename: str) -> dict:
+        '''
+        This static method reads the yaml file.
+
+        Args:
+            credentials_filename (str): the filename of the database
+            credentials.
+
+        Returns:
+            db_creds (dict): the database credentials.
+        '''
+        with open(f'{credentials_filename}.yaml', 'r') as file:
+            db_creds = yaml.safe_load(file)
+            return db_creds
