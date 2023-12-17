bff1a93fcd34d4ea9ddf682e6e1ad196a69000bf
Patrick Chan <q_p18@hotmail.com>
2023-12-17 20:00:28 +0000

    Refactor, abstract code to the Emulator class.

--- user_posting_emulation.py
+++ user_posting_emulation.py
@@ -1,14 +1,10 @@
-import requests
 import random
-import json
 import sqlalchemy
 
-from bson import json_util
-from sqlalchemy import text
 from sqlalchemy.engine.base import Engine
 from time import sleep
 
-from database_utils import FileReader
+from database_utils import FileReader, Emulator
 
 
 random.seed(100)
@@ -26,7 +22,7 @@ class AWSDBConnector:
         self.PASSWORD = self.db_creds['PASSWORD']
         self.DATABASE = self.db_creds['DATABASE']
         self.PORT = self.db_creds['PORT']
-        
+
     def create_db_connector(self) -> Engine:
         '''
         This method initialises the SQLAlchemy engine with the database
@@ -35,14 +31,15 @@ class AWSDBConnector:
         Returns:
             engine (Engine): the database engine.
         '''
-        engine = sqlalchemy.create_engine(f'mysql+pymysql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}?charset=utf8mb4')
+        engine = sqlalchemy.create_engine(
+            f'mysql+pymysql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}?charset=utf8mb4')
         return engine
 
 
 new_connector = AWSDBConnector()
 
 
-def run_infinite_post_data_loop():
+def run_infinite_post_data_loop() -> None:
     '''
     This method infinitely posts data from database to AWS Kafka topics.
     '''
@@ -57,88 +54,19 @@ def run_infinite_post_data_loop():
 
         with engine.connect() as connection:
 
-            pin_string = text(
-                f"SELECT * FROM pinterest_data LIMIT {random_row}, 1")
-            pin_selected_row = connection.execute(pin_string)
-            
-            for row in pin_selected_row:
-                pin_result = dict(row._mapping)
-                payload = json.dumps({
-                    'records': [
-                        {    
-                            'value': {
-                                'index': pin_result['index'], 
-                                'unique_id': pin_result['unique_id'],
-                                'title': pin_result['title'],
-                                'description': pin_result['description'],
-                                'poster_name': pin_result['poster_name'], 
-                                'follower_count': pin_result['follower_count'],
-                                'tag_list': pin_result['tag_list'],
-                                'is_image_or_video': 
-                                    pin_result['is_image_or_video'],
-                                'image_src': pin_result['image_src'], 
-                                'downloaded': pin_result['downloaded'],
-                                'save_location': pin_result['save_location'],
-                                'category': pin_result['category'],
-                            }
-                        }
-                    ]
-                }, default='json_serial')
-                headers = {'Content-Type': 'application/vnd.kafka.json.v2+json'}
-                response = requests.request(
-                    "POST", PIN_INVOKE_URL, headers=headers, data=payload)
-                print(response.status_code)
+            CONTENT_TYPE = 'application/vnd.kafka.json.v2+json'
 
-            geo_string = text(
-                f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
-            geo_selected_row = connection.execute(geo_string)
-            
-            for row in geo_selected_row:
-                geo_result = dict(row._mapping)
-                payload = json.dumps({
-                    'records': [
-                        {    
-                            'value': {
-                                'ind': geo_result['ind'], 
-                                'timestamp': geo_result['timestamp'],
-                                'latitude': geo_result['latitude'],
-                                'longitude': geo_result['longitude'],
-                                'country': geo_result['country'],
-                            }
-                        }
-                    ]
-                }, default=json_util.default)
-                headers = {'Content-Type': 'application/vnd.kafka.json.v2+json'}
-                response = requests.request(
-                    "POST", GEO_INVOKE_URL, headers=headers, data=payload)
-                print(response.status_code)
+            Emulator.ingest_data(
+                'pinterest_data', random_row, connection,
+                CONTENT_TYPE, PIN_INVOKE_URL)
 
-            user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
-            user_selected_row = connection.execute(user_string)
-            
-            for row in user_selected_row:
-                user_result = dict(row._mapping)
-                payload = json.dumps({
-                    'records': [
-                        {    
-                            'value': {
-                                'ind': user_result['ind'], 
-                                'first_name': user_result['first_name'],
-                                'last_name': user_result['last_name'],
-                                'age': user_result['age'],
-                                'date_joined': user_result['date_joined'],
-                            }
-                        }
-                    ]
-                }, default=json_util.default)
-                headers = {'Content-Type': 'application/vnd.kafka.json.v2+json'}
-                response = requests.request(
-                    "POST", USER_INVOKE_URL, headers=headers, data=payload)
-                print(response.status_code)
-            
-            print(pin_result)
-            print(geo_result)
-            print(user_result)
+            Emulator.ingest_data(
+                'geolocation_data', random_row, connection,
+                CONTENT_TYPE, GEO_INVOKE_URL)
+
+            Emulator.ingest_data(
+                'user_data', random_row, connection,
+                CONTENT_TYPE, USER_INVOKE_URL)
 
 
 if __name__ == "__main__":
--- user_posting_emulation_streaming.py
+++ user_posting_emulation_streaming.py
@@ -1,14 +1,10 @@
-import requests
 import random
-import json
 import sqlalchemy
 
-from bson import json_util
-from sqlalchemy import text
 from sqlalchemy.engine.base import Engine
 from time import sleep
 
-from database_utils import FileReader
+from database_utils import FileReader, Emulator
 
 
 random.seed(100)
@@ -41,8 +37,7 @@ class AWSDBConnector:
 
 new_connector = AWSDBConnector()
 
-
-def run_infinite_post_data_loop():
+def run_infinite_post_data_loop() -> None:
     '''
     This method infinitely posts data from database to AWS Kinesis.
     '''
@@ -56,86 +51,23 @@ def run_infinite_post_data_loop():
         USER_KINESIS_INVOKE_URL = config['USER_KINESIS_INVOKE_URL']
 
         with engine.connect() as connection:
+            
+            CONTENT_TYPE = 'application/json'
 
-            pin_string = text(
-                f"SELECT * FROM pinterest_data LIMIT {random_row}, 1")
-            pin_selected_row = connection.execute(pin_string)
-            
-            for row in pin_selected_row:
-                pin_result = dict(row._mapping)
-                payload = json.dumps({
-                    'StreamName': 'streaming-0a966c04ad33-pin',
-                    'Data': {
-                        'index': pin_result['index'], 
-                        'unique_id': pin_result['unique_id'],
-                        'title': pin_result['title'],
-                        'description': pin_result['description'],
-                        'poster_name': pin_result['poster_name'], 
-                        'follower_count': pin_result['follower_count'],
-                        'tag_list': pin_result['tag_list'],
-                        'is_image_or_video': 
-                            pin_result['is_image_or_video'],
-                        'image_src': pin_result['image_src'], 
-                        'downloaded': pin_result['downloaded'],
-                        'save_location': pin_result['save_location'],
-                        'category': pin_result['category'],
-                    },
-                    'PartitionKey': 'partition-1',
-                }, default='json_serial')
-                headers = {'Content-Type': 'application/json'}
-                response = requests.request(
-                    'PUT', PIN_KINESIS_INVOKE_URL, headers=headers, data=payload)
-                print(response.status_code)
-                print(response.text)
+            Emulator.ingest_data(
+                'pinterest_data', random_row, connection,
+                CONTENT_TYPE, PIN_KINESIS_INVOKE_URL,
+                True, 'streaming-0a966c04ad33-pin', 'partition-1')
 
-            geo_string = text(
-                f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
-            geo_selected_row = connection.execute(geo_string)
-            
-            for row in geo_selected_row:
-                geo_result = dict(row._mapping)
-                payload = json.dumps({
-                    'StreamName': 'streaming-0a966c04ad33-geo',
-                    'Data': {
-                        'ind': geo_result['ind'], 
-                        'timestamp': geo_result['timestamp'],
-                        'latitude': geo_result['latitude'],
-                        'longitude': geo_result['longitude'],
-                        'country': geo_result['country'],
-                    },
-                    'PartitionKey': 'partition-1',
-                }, default=json_util.default)
-                headers = {'Content-Type': 'application/json'}
-                response = requests.request(
-                    "PUT", GEO_KINESIS_INVOKE_URL, headers=headers, data=payload)
-                print(response.status_code)
-                print(response.text)
+            Emulator.ingest_data(
+                'geolocation_data', random_row, connection,
+                CONTENT_TYPE, GEO_KINESIS_INVOKE_URL,
+                True, 'streaming-0a966c04ad33-geo', 'partition-1')
 
-            user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
-            user_selected_row = connection.execute(user_string)
-            
-            for row in user_selected_row:
-                user_result = dict(row._mapping)
-                payload = json.dumps({
-                    'StreamName': 'streaming-0a966c04ad33-user',
-                    'Data': {
-                        'ind': user_result['ind'], 
-                        'first_name': user_result['first_name'],
-                        'last_name': user_result['last_name'],
-                        'age': user_result['age'],
-                        'date_joined': user_result['date_joined'],
-                    },
-                    'PartitionKey': 'partition-1',
-                }, default=json_util.default)
-                headers = {'Content-Type': 'application/json'}
-                response = requests.request(
-                    "PUT", USER_KINESIS_INVOKE_URL, headers=headers, data=payload)
-                print(response.status_code)
-                print(response.text)
-            
-            print(pin_result)
-            print(geo_result)
-            print(user_result)
+            Emulator.ingest_data(
+                'user_data', random_row, connection,
+                CONTENT_TYPE, USER_KINESIS_INVOKE_URL,
+                True, 'streaming-0a966c04ad33-user', 'partition-1')
 
 
 if __name__ == "__main__":
